b64f1e3a300eccc31b4e1d7f3518d15043f29cf5
Trey Shelton <tshelton@basecampcodingacademy.org>
2017-07-26 16:22:50 -0500

    began making my messages in the shell

---
+++ shell.py
@@ -0,0 +1,22 @@
+import core, disk
+
+def message():
+    print('\t!!Welcome to Trey\'s Dos Wheel Motorcycle Rental Agency!!\n\n')
+    print('How many days will you be needing\nto rent a "Dos Wheel Motorcycle"?\n**$139.99/day**\n')
+
+def tax_price_message():
+    import core.adding_tax()
+    print('Your price after taxes is', )    
+
+
+
+
+
+def main():
+    days = input(message())
+
+
+    
+if __name__ == '__main__':
+    main()
+

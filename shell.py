f194dbd94f7a4a2785f7a6301a49f8e75e686540
Trey Shelton <tshelton@basecampcodingacademy.org>
2017-07-27 13:34:35 -0500

    working on functions that use print and input returns. having trouble figuring out the format values i need

--- shell.py
+++ shell.py
@@ -1,19 +1,37 @@
 import core, disk
 
-def message():
-    print('\t!!Welcome to Trey\'s Dos Wheel Motorcycle Rental Agency!!\n\n')
-    print('How many days will you be needing\nto rent a "Dos Wheel Motorcycle"?\n**$139.99/day**\n')
+def greeting_message(inventory):
+    message = '\t!!Welcome to Trey\'s Dos Wheel Motorcycle Rental Agency!!\n\t\t\t**139.99 each day**\n\t\t\t     **7% tax**\n\t\t\t **10% damage fee**\n\t\t No damage? Get your damage fee back!\n\nChoose the type of motorcycle you would like to rent:\n'
+    for motorcycle in inventory.values():
+        message += ('{}. -> {} ({})\n'.format(motorcycle.get('code'), motorcycle.get('type_of_motorcycle'), motorcycle.get('color')))
+    message += 'Leave program = "Q" + "Enter"\n'
+    return message
 
-def tax_price_message():
-    import core.adding_tax()
-    print('Your price after taxes is', )    
+def get_amount_of_days(inventory):
+    message = greeting_message(inventory)
+    while True:
+        choice = input(message)
+        if choice.isalpha() == 'Q':
+            print('You have left the program.')
+            exit()
+        else:
+            input('---System Error--- ::\\ INVALID CHOICE')
+
+
 
 
 
 
 
 def main():
-    days = input(message())
+    keys, lines = disk.dos_inventory('inventory.txt')
+    inventory = core.inventory(keys, lines)
+    days = get_amount_of_days(inventory)
+    
+    
+
+
+
 
 
     

e948dfb8cc2e8c1f6ea9944e7deace126e4bec48
Trey Shelton <tshelton@basecampcodingacademy.org>
2017-07-28 14:16:18 -0500

    rearranged functions and parameters (as usual), also changed variables in my main()

--- shell.py
+++ shell.py
@@ -1,40 +1,45 @@
-import core, disk
+from core import *
+from disk import *
 
 def greeting_message(inventory):
-    message = '\t!!Welcome to Trey\'s Dos Wheel Motorcycle Rental Agency!!\n\t\t\t**139.99 each day**\n\t\t\t     **7% tax**\n\t\t\t **10% damage fee**\n\t\t No damage? Get your damage fee back!\n\nType the code of the motorcycle\nyou would like to rent:\n\n'
+    message = '\t!!Welcome to Trey\'s Dos Wheel Motorcycle Rental Agency!!\n\t\t\t**139.99 each day**\n\t\t\t     **7% tax**\n\t\t **10% damage fee added to price**\n\t\t No damage? Get your damage fee back!\n\nType the code of the motorcycle\nyou would like to rent:\n\n'
     for motorcycle in inventory.values():
-        message += ('{} -> {} ({})\n'.format(motorcycle.get('code'), motorcycle.get('type_of_motorcycle'), motorcycle.get('color')))
-    message += 'Leave program = "Q" + "Enter"\n'
-    return message
+        message += ('{} -> {} ({}): ${}\n'.format(motorcycle.get('code'), motorcycle.get('type_of_motorcycle'), motorcycle.get('color'), motorcycle.get('price')))
+    message += '\nLeave program = "Q" + "Enter"\n\n'
+    code = input(message)
+    return code
 
 def get_motorcycle(inventory):
-    message = greeting_message(inventory)
     while True:
-        choice = input(message)
-        if choice.isalpha() == 'Q':
+        choice = greeting_message(inventory)
+        if choice == 'Q':
             print('You have left the program.')
             exit()
+        elif choice in inventory.keys():
+            return choice
         else:
             print('---System Error--- ::\\ INVALID CHOICE\n')
 
-def get_days_message(code):
-    return '\nYou have chosen the {} rental motorcycle. How many\ndays would you like to rent this bike?'.format(code)
+def get_days_message(type_of_motorcycle,inventory,choice):
+    input('\nThe {} rental motorcycle is ${} before the 10%\ndamage fee. How many days would you like to rent this bike?\n'.format(type_of_motorcycle, inventory[choice]['price']))
 
-
-
-
+def total_charge(days, type_of_motorcycle):
 
 
 
 def main():
-    read_line, read_lines = disk.dos_inventory()
-    inventory = core.inventory(read_line, read_lines)
-    type_motorcycle = get_motorcycle(inventory)
-    
-    
+    i, inv = dos_inventory()
+    in_inventory = motorcycle_inventory(i, inv)
+    type_of_motorcycle = get_motorcycle(in_inventory)
+    pick = choose_motorcycle(in_inventory, type_of_motorcycle)
+    days = get_days_message(pick, in_inventory,type_of_motorcycle)
     
 
 
+
+    
+    
+    
 
 
 

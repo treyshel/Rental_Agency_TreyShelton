4b7c8ad159aa074bc35fd9b3f0a1f928c927e200
Trey Shelton <tshelton@basecampcodingacademy.org>
2017-07-31 16:24:28 -0500

    fixed choose_motorcycle function to reduce if and elif statements

--- core.py
+++ core.py
@@ -13,6 +13,13 @@ def motorcycle_inventory(i, inv):
         }
     return motorcycle
 
+def get_greeting_message(inventory):
+    message = '\t!!Welcome to Trey\'s Dos Wheel Motorcycle Rental Agency!!\n\n\n\t\t\t**139.99 each day**\n\t\t\t     **7% tax**\n\t  **3 FREE DAYS IF PURCHASED FOR 28 DAYS OR MORE**\n\t\t **10% damage fee added to price**\n\t\t No damage? Get your damage fee back!\n\nType the code of the motorcycle\nyou would like to rent:\n\n'
+    for motorcycle in inventory.values():
+        message += ('{} -> {} ({}): ${}\n'.format(motorcycle.get('code'), motorcycle.get('type_of_motorcycle'), motorcycle.get('color'), motorcycle.get('price')))
+    message += 'Leave program = "Q" + "Enter"\n\n'
+    return message
+
 def adding_tax(days):
     '''float -> float
     
@@ -42,8 +49,8 @@ def damage_deposit(choice, inventory):
         deposit = inventory[choice]['price'] * .10
         return round(deposit, 2)
 
-def damage_deposit_and_tax(deposit, total):
-    return deposit + total
+def damage_deposit_and_tax(deposit, amount):
+    return deposit + amount
 def valid_quantity(inventory, code, motorcycle):
     ''' {{'code': str, 'type_of_motorcycle': str, 'color': str, 'price': float, 'quantity': int}}   
     >>> types = {'0660': {'code': '0660', 'type': '2006 Suzuki GSXR 600', 'color': 'blue', 'price': 3499.99, 'quantity': 5}, '0410': {'code': '0410', 'type': '2004 Yamaha R1 1000', 'color': 'red', 'price': 3749.99, 'quantity': 9}}
@@ -54,20 +61,23 @@ def valid_quantity(inventory, code, motorcycle):
     '''
     return inventory.get(code).get('quantity', -1) >= motorcycle
 
-def choose_motorcycle(inventory, code):
-    '''str -> str'''
-    if code == '0660':
-        return '2006 Suzuki GSXR 600'
-    elif code == '0410':
-        return '2004 Yamaha R1 1000'
-    elif code == '0190':
-        return '2001 Kawasaki Ninja ZX-9R 900'
-    elif code == '1650':
-        return '2016 Honda CBR 500'
-    elif code == '1765':
-        return '2017 Kawasaki Ninja EX 650'
-    elif code == '0910':
-        return '2009 Suzuki GSX 1000 Hayabusa'
+def choose_motorcycle(in_inventory, code):
+    '''Item, str -> str'''
+    print(in_inventory)
+    return in_inventory[code]['type_of_motorcycle']
+    
+    # if code == '0660':
+    #     return '2006 Suzuki GSXR 600'
+    # elif code == '0410':
+    #     return '2004 Yamaha R1 1000'
+    # elif code == '0190':
+    #     return '2001 Kawasaki Ninja ZX-9R 900'
+    # elif code == '1650':
+    #     return '2016 Honda CBR 500'
+    # elif code == '1765':
+    #     return '2017 Kawasaki Ninja EX 650'
+    # elif code == '0910':
+    #     return '2009 Suzuki GSX 1000 Hayabusa'
 
 
 

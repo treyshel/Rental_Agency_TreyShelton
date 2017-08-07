aa8f86882e84fb677707b938845df1348728d729
Trey Shelton <tshelton@basecampcodingacademy.org>
2017-08-07 14:51:31 -0500

    moving alot of things around to make a customer main and an employee main

--- shell.py
+++ shell.py
@@ -1,8 +1,7 @@
 from core import *
 from disk import *
-import time
-
-
+import time        
+    
 def get_motorcycle(inventory, message):
     while True:
         code = input(message)
@@ -27,45 +26,66 @@ def name_for_return():
     return name
 
 def return_message(pick, name, total):
-    print('\nName/code: {}\nYour Dos Motorcycle: {}\nTaxes: 7% of days (139.99/day)\nDamage Deposit: 10% of Motorcycle Cost\nYour total will be ${:.2f}'.format(name, pick, total))
+    print('\nName/code: {}\nYour Dos Motorcycle: {}\nTaxes: 7% of days ($139.99/day)\nDamage Deposit: 10% of Motorcycle Cost\nYour total will be ${:.2f}\n\nHAVE A GREAT DAY!'.format(name, pick, total))
 
-def return_day(name, return_name):
+def return_decision():
     while True:
+        return_name = input('We hope you had a great experience with our Dos\nMotorcycle! What was the name for your rental under?\n')
         if return_name == name:
-            print('Okay, it looks like you rented the {}. Here\nis your return deposit of ${:.2f}, have a great day!'.format(pick, deposit))
+            print('Let me type it in the system and see if we can find it for you.\n')
+            time.sleep(1.5)
+            print('Here we go! It looks like you rented the {}. Here\nis your return deposit of ${:.2f}!\n\nHAVE A GREAT DAY!'.format(pick, deposit))
             break
         else:
             input('\nI\'m sorry but this isn\'t a return name/code. Try again:\n')
-            continue
+            continue 
+
+def customer_decision():
+    while True:
+        decision = input('\nHey there! What will you be needing today?\n\n*Press 1 to rent a Dos Wheel Motorcycle\n*Press 2 to return a Dos Wheel Motorcycle\n')
+        if decision == '1':
+            i, inv = dos_inventory()
+            in_inventory = motorcycle_inventory(i, inv)
+            greeting_message = get_greeting_message(in_inventory)
+            code = get_motorcycle(in_inventory, greeting_message)
+            pick = choose_motorcycle(in_inventory, code)
+            days = get_days_message(pick, in_inventory, code)
+            name = name_for_return()
+            deposit = damage_deposit(code, in_inventory)
+            amount = adding_tax(int(days))
+            total = damage_deposit_and_tax(deposit, amount)
+            return_message(pick, name, total)
+            quantity_take_away(in_inventory, code)
+            in_history(name, code, days, total)
+            break
+        elif decision == '2':
+            while True:
+                return_name = input('We hope you had a great experience with our Dos\nMotorcycle! What was the name for your rental under?\n')
+                if return_name == name:
+                    print('Let me type it in the system and see if we can find it for you.\n')
+                    time.sleep(1.5)
+                    print('Here we go! It looks like you rented the {}. Here\nis your return deposit of ${:.2f}!\n\nHAVE A GREAT DAY!'.format(pick, deposit))
+                    break
+                else:
+                    input('\nI\'m sorry but this isn\'t a return name/code. Try again:\n')
+                    continue 
+        else:
+            print('Invalid choice. Please choose a number: "1" or "2"')
+
+
+        
+
 
 def main():
-    i, inv = dos_inventory()
-    in_inventory = motorcycle_inventory(i, inv)
-    greeting_message = get_greeting_message(in_inventory)
-    code = get_motorcycle(in_inventory, greeting_message)
-    pick = choose_motorcycle(in_inventory, code)
-    days = get_days_message(pick, in_inventory, code)
-    name = name_for_return()
-    deposit = damage_deposit(code, in_inventory)
-    amount = adding_tax(int(days))
-    total = damage_deposit_and_tax(deposit, amount)
-    return_message(pick, name, total)
-    quantity_take_away(in_inventory, code)
-    in_history(name, code, days, total)
-    print('----------------------------------------------------------------------------------\n\n*RETURN DAY*\n\n\n')
-    return_name = input('Hello! We hope you had a great experience with our Dos\nMotorcycle! What was the name for your rental under?\n')
-    return_day(name, return_day)
-    
-    
+    while True:
+        customer_or_employee = input('Are you a customer or an employee?\n')
+        if customer_or_employee.title().strip() == 'customer'.title().strip():
+            return customer_decision()
+        else:
+            print('Please type customer or employee to continue.\n')
+        
 
 
-    
-    
-    
-
-
-
-    
 if __name__ == '__main__':
     main()
 

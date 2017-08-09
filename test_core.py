668a24b26700b1d131800c0897061d924ea8f803
Trey Shelton <tshelton@basecampcodingacademy.org>
2017-08-09 16:17:16 -0500

    added more tests to my test core

--- test_core.py
+++ test_core.py
@@ -31,27 +31,6 @@ def test_damage_deposit_and_tax():
     assert damage_deposit_and_tax(299.60, 279.98) == 579.58
 
 
-def test_valid_quantity():
-    types = {
-        '0660': {
-            'code': '0660',
-            'type_of_motorcycle': '2006 Suzuki GSXR 600',
-            'color': 'blue',
-            'price': 3499.99,
-            'quantity': 5
-        },
-        '0410': {
-            'code': '0410',
-            'type_of_motorcycle': '2004 Yamaha R1 1000',
-            'color': 'red',
-            'price': 3749.99,
-            'quantity': 9
-        }
-    }
-    assert valid_quantity(types, '0660', 1) == True
-    assert valid_quantity(types, '0410', 10) == False
-
-
 def test_choose_motorcycle():
     inventory = {
         '0660': {
@@ -71,3 +50,29 @@ def test_choose_motorcycle():
     }
     assert choose_motorcycle(inventory, '0660') == '2006 Suzuki GSXR 600'
     assert choose_motorcycle(inventory, '0410') == '2004 Yamaha R1 1000'
+
+
+def test_motorcycle_inventory():
+    inventory = {
+        '0660': {
+            'code': '0660',
+            'type_of_motorcycle': '2006 Suzuki GSXR 600',
+            'color': 'blue',
+            'price': 3499.99,
+            'quantity': 5
+        },
+        '0410': {
+            'code': '0410',
+            'type_of_motorcycle': '2004 Yamaha R1 1000',
+            'color': 'red',
+            'price': 3749.99,
+            'quantity': 9
+        }
+    }
+    assert motorcycle_inventory('0410', inventory) == {
+        'code': '0410',
+        'type_of_motorcycle': '2004 Yamaha R1 1000',
+        'color': 'red',
+        'price': 3749.99,
+        'quantity': 9
+    }
